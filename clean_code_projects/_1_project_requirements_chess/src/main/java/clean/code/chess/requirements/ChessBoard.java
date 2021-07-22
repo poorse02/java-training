54e5adb50939963f59a2d8d522697acd2c4edfa1
Tae Andreea-Cristina <tae.andreea@gmail.com>
2021-07-22 14:05:46 +0300

    Proiect 2 - Chess

--- clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/ChessBoard.java
+++ clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/ChessBoard.java
@@ -1,22 +1,105 @@
 package clean.code.chess.requirements;
 
+import java.util.ArrayList;
+
 public class ChessBoard {
 
     public static int MAX_BOARD_WIDTH = 7;
     public static int MAX_BOARD_HEIGHT = 7;
 
     private Pawn[][] pieces;
+    private ArrayList<Pawn> whitePawns = new ArrayList<>();
+    private ArrayList<Pawn> blackPawns = new ArrayList<>();
 
     public ChessBoard() {
         pieces = new Pawn[MAX_BOARD_WIDTH][MAX_BOARD_HEIGHT];
 
     }
 
+    public ArrayList<Pawn> getWhitePawnList(){
+        return whitePawns;
+    }
+
+    public void setWhitePawns(ArrayList<Pawn> array){
+        this.whitePawns = array;
+    }
+
+    public ArrayList<Pawn> getBlackPawnList(){
+        return blackPawns;
+    }
+
+    public void setBlackPawns(ArrayList<Pawn> array){
+        this.blackPawns = array;
+    }
+
+
     public void Add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.add()");
+            if (this.IsLegalBoardPosition(xCoordinate,yCoordinate)
+                    && this.checkIfPositionFree(xCoordinate,yCoordinate)){
+                System.out.println("~~~~~~~~~~~");
+                System.out.println("Adding pawn: ");
+                System.out.println("Desired position " + xCoordinate + ", " +yCoordinate);
+
+                pawn.setXCoordinate(xCoordinate);
+                System.out.println("position x set " + pawn.getXCoordinate());
+                pawn.setYCoordinate(yCoordinate);
+                System.out.println("position y set " + pawn.getYCoordinate());
+                pawn.setChessBoard(this);
+
+                if(pieceColor == PieceColor.BLACK && this.getBlackPawnList().size() < MAX_BOARD_WIDTH) {
+                    blackPawns.add(pawn);
+
+                }
+
+                else if(pieceColor == PieceColor.WHITE && this.getBlackPawnList().size() < MAX_BOARD_WIDTH)
+                    whitePawns.add(pawn);
+                else{
+                    pawn.setXCoordinate(-1);
+                    pawn.setYCoordinate(-1);
+
+            }
+
+            }
+            System.out.println("after adding: ");
+            for(int i = 0 ; i < blackPawns.size() ; i++){
+                System.out.println("piece at position "+ i + " w/ coordinates: "+
+                        blackPawns.get(i).getXCoordinate()+", "+ blackPawns.get(i).getYCoordinate());
+        }
+
     }
 
     public boolean IsLegalBoardPosition(int xCoordinate, int yCoordinate) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.IsLegalBoardPosition()");
+        if ( xCoordinate >= 0 && xCoordinate<=7 && yCoordinate >=0 && yCoordinate <=7)
+            return true;
+        else return false;
+    }
+
+    public boolean checkIfPositionFree(int xCoordinate, int yCoordinate){
+        System.out.println("~~~~~~~");
+        System.out.println("Check if position free: ");
+        //check white pawns
+        int check = 0;
+        for (int i = 0; i <whitePawns.size();i++){
+            if(whitePawns.get(i).getXCoordinate() == xCoordinate || whitePawns.get(i).getYCoordinate() == yCoordinate){
+                check = 1;
+                System.out.println("white pawn number " + i + "has coordinates: "+ whitePawns.get(i).getXCoordinate()
+                        + ", " + whitePawns.get(i).getYCoordinate());
+            }
+
+        }
+
+        //check black pawns
+        for (int i = 0; i <blackPawns.size();i++){
+            if(blackPawns.get(i).getXCoordinate() == xCoordinate || blackPawns.get(i).getYCoordinate() == yCoordinate){
+                System.out.println("black pawn number " + i + "has coordinates: "+ blackPawns.get(i).getXCoordinate()
+                        + ", " + blackPawns.get(i).getYCoordinate());
+                check = 1;
+            }
+
+        }
+
+        if(check == 1)
+            return false;
+        else return true;
     }
 }
--- clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/Pawn.java
+++ clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/Pawn.java
@@ -11,7 +11,7 @@ public class Pawn {
         this.pieceColor = pieceColor;
     }
 
-    public ChessBoard getChesssBoard() {
+    public ChessBoard getChessBoard() {
         return chessBoard;
     }
 
@@ -44,8 +44,84 @@ public class Pawn {
     }
 
     public void Move(MovementType movementType, int newX, int newY) {
-        throw new UnsupportedOperationException("Need to implement Pawn.Move()") ;
+//        throw new UnsupportedOperationException("Need to implement Pawn.Move()") ;
+
+        //move, but only if the position is free and if the position is valid\
+        if(movementType == MovementType.MOVE) {
+            System.out.println("~~~~~~~~~~~~");
+            System.out.println("Moving");
+            System.out.println("    Wanted positions:" + newX + ", " + newY);
+            System.out.println("    is new position free: "+this.checkIfPositionFree(newX, newY));
+            System.out.println("    is new position legal: "+this.chessBoard.IsLegalBoardPosition(newX, newY));
+            this.checkIfPositionFree(newX,newY);
+
+
+            //important part
+            //we define black pawns being on top (rows 6 & 7) and white on the bottom (rows 0 & 1) initially
+            if(this.getPieceColor() == PieceColor.BLACK){
+                if (this.checkIfPositionFree(newX, newY) && this.chessBoard.IsLegalBoardPosition(newX, newY)
+                    && newY < this.getYCoordinate() && newX == this.getXCoordinate()) {
+                    this.setXCoordinate(newX);
+                    this.setYCoordinate(newY);
+                }
+            }
+
+            if(this.getPieceColor() == PieceColor.WHITE){
+                if (this.checkIfPositionFree(newX, newY) && this.chessBoard.IsLegalBoardPosition(newX, newY)
+                        && newY > this.getYCoordinate() && newX == this.getXCoordinate()) {
+                    this.setXCoordinate(newX);
+                    this.setYCoordinate(newY);
+                }
+            }
+
+            //
+
+            if (!this.checkIfPositionFree(newX, newY)) {
+                System.out.println("    Checking other occupied positions: ");
+                System.out.println("        black pawns");
+                for (int i = 0; i < this.chessBoard.getBlackPawnList().size(); i++) {
+                    System.out.print("          position for "+ i + " piece: ");
+                    System.out.print(this.chessBoard.getBlackPawnList().get(i).getXCoordinate());
+                    System.out.println(" " + this.chessBoard.getBlackPawnList().get(i).getYCoordinate());
+                }
+
+
+                System.out.println("        white pawns");
+                for (int i = 0; i < this.chessBoard.getWhitePawnList().size(); i++) {
+                    System.out.print("          position for "+ i + " piece: ");
+                    System.out.print(this.chessBoard.getWhitePawnList().get(i).getXCoordinate());
+                    System.out.println(" " + this.chessBoard.getWhitePawnList().get(i).getYCoordinate());
+                }
+
+            }
+        }
+
     }
+
+    public boolean checkIfPositionFree(int xCoordinate, int yCoordinate){
+        //check white pawns
+        int check = 0;
+        for (int i = 0; i <this.chessBoard.getWhitePawnList().size();i++){
+            if((this.chessBoard.getWhitePawnList().get(i).getXCoordinate() == xCoordinate && this.getXCoordinate() != xCoordinate)
+                    || this.chessBoard.getWhitePawnList().get(i).getYCoordinate() == yCoordinate && this.getYCoordinate() != yCoordinate )
+                check = 1;
+        }
+
+        //check black pawns
+        for (int i = 0; i <this.chessBoard.getBlackPawnList().size();i++){
+            if(this.chessBoard.getBlackPawnList().get(i).getXCoordinate() == xCoordinate
+                    || this.chessBoard.getBlackPawnList().get(i).getYCoordinate() == yCoordinate)
+                check = 1;
+        }
+
+        if(check == 1)
+            return false;
+        else return true;
+    }
+
+
+
+
 
     @Override
     public String toString() {
